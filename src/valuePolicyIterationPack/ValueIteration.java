e1abf66862ca8335f3ec95988ae1ca958d17bbc5
GiorgosMethe <giorgosmethe@gmail.com>
2012-09-15 16:33:17 +0200

    value iteration redesigned

--- src/valuePolicyIterationPack/ValueIteration.java
+++ src/valuePolicyIterationPack/ValueIteration.java
@@ -19,7 +19,7 @@ public class ValueIteration {
 
 	}
 
-	public static void ValueIteration11() {
+	public static void ValueIteration11NEw() {
 		// TODO Auto-generated method stub
 		double[][] State = new double[11][11];
 
@@ -30,16 +30,22 @@ public class ValueIteration {
 
 		double gamma = 0.7;
 		double delta;
+		double ValuePrevious;
 		int cnt = 0;
 
 		do {
 
 			delta = 0;
 			cnt++;
+			System.out.println();
+			System.out.println("--new swift");
+			System.out.println();
 			for (int i = 0; i < State.length; i++) {
 				for (int j = 0; j < State.length; j++) {
+					
+					//System.out.println("++new state");
 
-					double ValuePrevious = State[i][j];
+					ValuePrevious = State[i][j];
 
 					Environment env = new Environment();
 					Predator P = new Predator("Predator", new Coordinate(i, j),
@@ -52,46 +58,52 @@ public class ValueIteration {
 							.ProbabilityActions(env.worldState);
 					Vector<RandomAction> PossiblePreyActions = p
 							.ProbabilityActions(env.worldState);
+					
+					if (rewardFunction(P.position, p.position) > 0) {
 
-					double max = 0;
-					double ValueCurrent = 0;
-					double finalValue = 0;
+						State[i][j] = rewardFunction(P.position, p.position);
 
-					for (int ii = 0; ii < PossiblePredatorActions.size(); ii++) {
+					} else {
 
-						ValueCurrent = 0;
+						double max = 0;
+						for (int ii = 0; ii < PossiblePredatorActions.size(); ii++) {
+							double ValueCurrent = 0;
+							for (int jj = 0; jj < PossiblePreyActions.size(); jj++) {
 
-						for (int jj = 0; jj < PossiblePreyActions.size(); jj++) {
+								double prob = PossiblePredatorActions
+										.elementAt(ii).prob
+										* PossiblePreyActions.elementAt(jj).prob;
 
-							// probability of each s-prime state
-							double Prob = PossiblePredatorActions.elementAt(ii).prob
-									* PossiblePreyActions.elementAt(jj).prob;
+								double reward = rewardFunction(
+										PossiblePredatorActions.elementAt(ii).coordinate,
+										PossiblePreyActions.elementAt(jj).coordinate);
 
-							double Reward = rewardFunction(
-									P.position,
-									PossiblePreyActions.elementAt(jj).coordinate);
+								double discount = gamma
+										* State[PossiblePredatorActions
+												.elementAt(ii).coordinate
+												.getX()][PossiblePredatorActions
+												.elementAt(ii).coordinate
+												.getY()];
 
-							double Discount = gamma
-									* State[PossiblePredatorActions
-											.elementAt(ii).coordinate.getX()][PossiblePredatorActions
-											.elementAt(ii).coordinate.getY()];
+								ValueCurrent += prob * (reward + discount);
+							}
 
-							ValueCurrent += Prob * (Reward + Discount);
-
+							max = Math.max(max, ValueCurrent);
+							
+							
 						}
-
-						max = Math.max(max, ValueCurrent);
-
-					}
-
-					State[i][j] = max;
-
-					delta = Math.max(delta, Math.abs(max - ValuePrevious));
-
+						
+						State[i][j] = max;
+						delta = Math.max(delta, Math.abs(max-ValuePrevious));
+						
+						
+					}					
 				}
 			}
+			
+			System.out.println(delta);
 
-		} while (delta > 0.1);
+		} while (delta > Math.pow(10, -4));
 
 		for (int i = 0; i < State.length; i++) {
 			System.out.println();
@@ -112,70 +124,6 @@ public class ValueIteration {
 
 		return 0;
 
-	}
-
-	public static void ValueIteration11NEw() {
-		// TODO Auto-generated method stub
-		double[][] State = new double[11][11];
-
-		// initialize the array
-		for (int i = 0; i < State.length; i++)
-			for (int j = 0; j < State.length; j++)
-				State[i][j] = 0.0;
-
-		double gamma = 0.7;
-		double delta;
-		int cnt = 0;
-
-		do {
-
-			delta = 0;
-			cnt++;
-			for (int i = 0; i < State.length; i++) {
-				for (int j = 0; j < State.length; j++) {
-
-					System.out.println("------------");
-					System.out.println("" + i + " " + j);
-					Environment env = new Environment();
-					Predator P = new Predator("Predator", new Coordinate(i, j),
-							null);
-					Prey p = new Prey("prey", new Coordinate(5, 5), null);
-					env.addAgent(P);
-					env.addAgent(p);
-
-					Vector<RandomAction> PossiblePredatorActions = P
-							.ProbabilityActions(env.worldState);
-					Vector<RandomAction> PossiblePreyActions = p
-							.ProbabilityActions(env.worldState);
-
-					System.out.println("PossibleWorldStates: "
-							+ PossiblePredatorActions.size()
-							* PossiblePreyActions.size());
-					double prob = 0;
-					for (int ii = 0; ii < PossiblePredatorActions.size(); ii++) {
-						for (int jj = 0; jj < PossiblePreyActions.size(); jj++) {
-							System.out.println("SumProbabilityPrey:"
-									+ PossiblePreyActions.elementAt(jj).prob);
-						}
-					}
-					System.out.println("SumProbability:" + prob);
-
-					System.out.println("------------");
-
-				}
-			}
-
-		} while (delta > 0.1);
-
-		for (int i = 0; i < State.length; i++) {
-			System.out.println();
-			for (int j = 0; j < State.length; j++) {
-				System.out.print((float) State[i][j] + " ");
-			}
-		}
-
-		System.out.println("\nDelta = " + delta);
-		System.out.println("\ncnt " + cnt);
 	}
 
 }
