0763fdf39904b0d50997313e2161cf5a61995f08
Patrick de Kok <patrick@cxiu.nl>
2012-09-26 18:10:22 +0200

    Started working on the QLearning.

--- src/agentsPack/Policy.java
+++ src/agentsPack/Policy.java
@@ -1,33 +1,14 @@
 package agentsPack;
 
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Vector;
 
 import environmentPack.Coordinate;
 
-public abstract class Policy {
-	protected Vector<Double> functionV = new Vector<Double>();
-
-	// sets functionV with appropriate values
-	public abstract void generateV();
-
-	public abstract Integer stateIndex(Vector<Agent> worldState);
-
-	public abstract Map<Coordinate, Double> getActions(Agent me,
-			Vector<Agent> worldState, Vector<Coordinate> possibleActions);
-
-	public Coordinate getOptimalAction(Agent me, Vector<Agent> worldState,
-			Vector<Coordinate> possibleActions) {
-		Map<Coordinate, Double> functionQ = getActions(me, worldState,
-				possibleActions);
-		Double maxValue = null;
-		Coordinate maxAction = null;
-		for (Map.Entry<Coordinate, Double> actionValue : functionQ.entrySet()) {
-			if (maxValue == null || actionValue.getValue() >= maxValue) {
-				maxValue = actionValue.getValue();
-				maxAction = actionValue.getKey();
-			}
-		}
-		return maxAction;
-	}
+public interface Policy {
+	public Map<Coordinate, Double> getActions(Agent me, 
+			Vector<Agent> worldState, 
+			Vector<Coordinate> possibleActions, 
+			Map<Entry<Vector<Agent>, Coordinate>, Double> qTable);
 }
---
+++ src/agentsPack/QPredator.java
@@ -0,0 +1,54 @@
+package agentsPack;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Vector;
+
+import actionPack.RandomAction;
+
+import environmentPack.Coordinate;
+
+public class QPredator extends Predator {
+	
+	protected Map<Entry<Vector<Agent>, Coordinate>, Double> q;
+
+	public QPredator(String name, Coordinate p, Policy pi, Map<Entry<Vector<Agent>, Coordinate>, Double> qInitialization) {
+		super(name, p, pi);
+		if (qInitialization == null) {
+			this.q = new DefaultHashMap<Map.Entry<Vector<Agent>,Coordinate>, Double>(Double.NEGATIVE_INFINITY);
+		}
+		else {
+			this.q = qInitialization;
+		}
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public Coordinate doAction(Vector<Agent> worldState) {
+		// Choose an action according to this.pi, and learn on-line
+		return null;
+	}
+	
+	public void receiveReward(Double r) {
+		// pass it to QLearning
+	}
+	
+	@Override
+	public Double reward(Vector<Agent> currState, Vector<Agent> nextState, Coordinate action) {
+		throw new UnsupportedOperationException("Sorry, you don't know the reward function. Receive one from the environment.");
+	}
+	
+	@Override
+	public Map<Integer, Double> functionP(Vector<Agent> worldState) {
+		throw new UnsupportedOperationException("Sorry, you should use ProbabilityActionsRSW(worldState).");
+	}
+	@Override
+	public Vector<RandomAction> ProbabilityActions(Vector<Agent> worldState) {
+		throw new UnsupportedOperationException("Sorry, we only use a really small world.");
+	}
+
+	@Override
+	public Vector<RandomAction> ProbabilityActionsSW(Vector<Agent> worldState) {
+		throw new UnsupportedOperationException("Sorry, we only use a really small world.");
+	}
+}
