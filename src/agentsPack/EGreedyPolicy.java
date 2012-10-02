40170995e3617c1a470594f688624ad252aceca6
Patrick de Kok <patrick@cxiu.nl>
2012-10-02 15:31:32 +0200

    Return of the EGreedyPolicy; a new hope.

---
+++ src/agentsPack/EGreedyPolicy.java
@@ -0,0 +1,59 @@
+package agentsPack;
+
+import java.util.Map;
+import java.util.Vector;
+import java.util.Map.Entry;
+import agentsPack.Agent;
+import agentsPack.DefaultHashMap;
+import environmentPack.Coordinate;
+import agentsPack.Policy;
+
+
+public class EGreedyPolicy implements Policy {
+
+	public double epsilon;
+
+	public EGreedyPolicy(Double epsilon) {
+		this.epsilon=epsilon;
+	}
+
+
+	@Override
+	public Coordinate chooseAction(Vector<Agent> worldState, QTable qTable){
+		Double r = Math.random();
+		Coordinate maxAction = null;
+		Double maxValue = Double.NEGATIVE_INFINITY;
+
+		for(Entry<Coordinate, Double> e : qTable.get(worldState).entrySet()){
+			if(e.getValue() > maxValue) {
+				maxValue = e.getValue();
+				maxAction = e.getKey();
+			}
+		}
+
+		if (r <= epsilon) {
+			Map<Coordinate, Double> actionValueMap = new DefaultHashMap<Coordinate, Double>(qTable.get(worldState), 0.0);
+			actionValueMap.remove(maxAction);
+			Double step = (1 - epsilon) / actionValueMap.size();
+			Double counter = step;
+			for (Coordinate a : actionValueMap.keySet()) {
+				if (counter <= r) {
+					return a;
+				}
+				counter += step;
+			}
+		}
+		return maxAction;
+
+	}
+
+
+	public double getEpsilon() {
+		return epsilon;
+	}
+
+
+	public void setEpsilon(double epsilon) {
+		this.epsilon = epsilon;
+	}
+}
