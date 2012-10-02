a74403c11e6492cb23ce199ae88cbbae9c8bf9c4
Patrick de Kok <patrick@cxiu.nl>
2012-10-02 17:20:15 +0200

    Needed for off-policy MC. There is a small difference between random and arbitrary.

---
+++ src/agentsPack/ArbitraryPolicy.java
@@ -0,0 +1,36 @@
+package agentsPack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+import environmentPack.Coordinate;
+
+public class ArbitraryPolicy implements Policy {
+
+	protected Map<Vector<Agent>, Coordinate> arbitraryActions;
+
+	// Arbitrary = every time you look up the action for s, you pick the same random action
+	public ArbitraryPolicy() {
+		this.arbitraryActions = new HashMap<Vector<Agent>, Coordinate>();
+	}
+
+	@Override
+	public Coordinate chooseAction(Vector<Agent> worldState, QTable qTable) {
+		if (!this.arbitraryActions.containsKey(worldState)) {
+			Predator pred = null;
+			for (Agent a : worldState) {
+				if (a instanceof Predator) {
+					pred = (Predator) a;
+				}
+			}
+			if (pred == null) {
+				throw new NullPointerException("No Predator in the worldState?");
+			}
+			Vector<Coordinate> possibleActions = pred.possibleActions(worldState);
+			this.arbitraryActions.put(worldState, possibleActions.get((int) (Math.random() * possibleActions.size())));
+		}
+		return this.arbitraryActions.get(worldState);
+	}
+
+}
