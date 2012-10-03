4cc252cd183da71aff5535de2c491f4b0f1e32d8
Patrick de Kok <patrick@cxiu.nl>
2012-10-03 02:03:12 +0200

    Needed the strange Factory-construction for this :D

--- src/agentsPack/QTable.java
+++ src/agentsPack/QTable.java
@@ -1,5 +1,6 @@
 package agentsPack;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Vector;
 
@@ -9,15 +10,14 @@ import agentsPack.Agent;
 import agentsPack.DefaultHashMap;
 
 public class QTable extends DefaultHashMap<Vector<Agent>, Map<Coordinate, Double>> {
-
+	
 	/**
 	 * This made Eclipse very happy.
 	 */
 	private static final long serialVersionUID = 8796256036751217618L;
 
 	public QTable(Double initialValue) {
-		super(new DefaultHashMap<Coordinate, Double>(5, initialValue));
-		// TODO Auto-generated constructor stub
+		super(new StateActionDefault(initialValue));
 	}
 	
 	public QTable(Map<Coordinate, Double> initialValues) {
@@ -25,18 +25,39 @@ public class QTable extends DefaultHashMap<Vector<Agent>, Map<Coordinate, Double
 	}
 
 	public QTable(int initialCapacity, Double initialValue) {
-		super(initialCapacity, new DefaultHashMap<Coordinate, Double>(5, initialValue));
+		super(initialCapacity, new StateActionDefault(initialValue));
 		// TODO Auto-generated constructor stub
 	}
 
 	public QTable(Map<Vector<Agent>, Map<Coordinate, Double>> copyTable, Double initialValue) {
-		super(copyTable, new DefaultHashMap<Coordinate, Double>(5, initialValue));
+		super(copyTable, new StateActionDefault(initialValue));
 		// TODO Auto-generated constructor stub
 	}
 
 	public QTable(int initialCapacity, float loadFactor, Double initialValue) {
-		super(initialCapacity, loadFactor, new DefaultHashMap<Coordinate, Double>(5, initialValue));
+		super(initialCapacity, loadFactor, new StateActionDefault(initialValue));
 		// TODO Auto-generated constructor stub
+	}
+	
+	public Map<Coordinate, Double> get(Vector<Agent> worldState) {
+//		if (worldState.size() <= 1 ||
+//				Coordinate.compareCoordinates(worldState.get(0).position, worldState.get(1).position)) {
+//			return new DefaultHashMap<Coordinate, Double>(0.0);
+//		}
+//		if (!this.containsKey(worldState)) {
+//			Map<Coordinate, Double> defaultMap = new HashMap<Coordinate, Double>();
+//			Agent predator = null;
+//			for (Agent a : worldState) {
+//				if (a instanceof Predator) {
+//					predator = a;
+//				}
+//			}
+//			for (Coordinate action : predator.possibleActions(worldState)) {
+//				defaultMap.put(action, this.initialValue);
+//			}
+//			this.put(worldState, defaultMap);
+//		}
+		return super.get(worldState);
 	}
 	
 	public Double get(Vector<Agent> worldState, Coordinate action) {
@@ -74,4 +95,29 @@ public class QTable extends DefaultHashMap<Vector<Agent>, Map<Coordinate, Double
 		return this.getString(11*11);
 	}
 
+}
+
+class StateActionDefault implements DefaultHashMap.DefaultFactory<Vector<Agent>, Map<Coordinate, Double>> {
+	public Double initialValue;
+	
+	public StateActionDefault(Double initialValue) {
+		this.initialValue = initialValue;
+	}
+	@Override
+	public Map<Coordinate, Double> getDefault(Vector<Agent> worldState) {
+		if (worldState.size() <= 1 || Coordinate.compareCoordinates(worldState.get(0).position, worldState.get(1).position)) {
+			return new DefaultHashMap<Coordinate, Double>(0.0);
+		}
+		Map<Coordinate, Double> defaultMap = new DefaultHashMap<Coordinate, Double>(this.initialValue);
+		Agent predator = null;
+		for (Agent a : worldState) {
+			if (a instanceof Predator) {
+				predator = a;
+			}
+		}
+		for (Coordinate action : predator.possibleActions(worldState)) {
+			defaultMap.put(action, this.initialValue);
+		}
+		return defaultMap;
+	}	
 }
