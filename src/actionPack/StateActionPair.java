b74b3fd952ac16b70ebf22ce798e36aabfa6d415
Patrick de Kok <patrick@cxiu.nl>
2012-10-04 02:36:45 +0200

    I thought I would need an equals on this, but then, after implementing I thought NO MAPS!

--- src/actionPack/StateActionPair.java
+++ src/actionPack/StateActionPair.java
@@ -15,4 +15,27 @@ public class StateActionPair {
 		this.id = id;
 
 	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof StateActionPair)) {
+			return false;
+		}
+		StateActionPair that = (StateActionPair) o;
+		
+		return (that == null) 
+				|| ((this.Action == null ? that.Action == null : that.Action.equals(this.Action) 
+					|| (this.Value == that.Value)
+					|| (this.id == that.id))); 
+	}
+	
+	@Override
+	public int hashCode() {
+		int hashCode = 0;
+		if (this.Action != null) {
+			hashCode += 11 * this.Action.getX() + this.Action.getY(); 
+		}
+		hashCode = id * ((int) this.Value) ^ hashCode;
+		return hashCode;
+	}
 }
