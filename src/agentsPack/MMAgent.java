b1f3648185d5be915299e2e78dff63de4f72c224
GiorgosMethe <giorgosmethe@gmail.com>
2012-10-16 17:34:12 +0200

    Q TABLE UPDATE

--- src/agentsPack/MMAgent.java
+++ src/agentsPack/MMAgent.java
@@ -27,7 +27,7 @@ public class MMAgent extends Agent {
 
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	@Override
 	public Integer stateIndex(Vector<Agent> worldState) {
 		// TODO Auto-generated method stub
@@ -82,7 +82,7 @@ public class MMAgent extends Agent {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public void initializeQtable(Vector<Agent> worldstate) {
 
@@ -92,69 +92,80 @@ public class MMAgent extends Agent {
 		this.vTable = new double[QtableSize];
 		// find my position on the world state vector
 		int mySelf = WhoAmI(worldstate);
-		System.out.println("eimai to pred "+mySelf);
+		System.out.println("eimai to pred " + mySelf);
 		for (int i = 0; i < this.qTable.length; i++) {
 			// add my actions in this state
 			// my position is given through the following:
 			Coordinate MyState = IndexToMyPos(i, mySelf);
 			Coordinate OtherAgentState;
-			if(mySelf==0){
+			if (mySelf == 0) {
 				OtherAgentState = IndexToMyPos(i, 1);
-			}else{
+			} else {
 				OtherAgentState = IndexToMyPos(i, 0);
 			}
-			//initialize pi table
+			// initialize pi table
 			this.piTable[i] = new Vector<StateActionPair>();
 			this.piTable[i].add(new StateActionPair(MyState.getEast(), 0.2, 1));
-			this.piTable[i].add(new StateActionPair(MyState.getNorth(), 0.2, 2));
-			this.piTable[i].add(new StateActionPair(MyState.getSouth(), 0.2, 3));
+			this.piTable[i]
+					.add(new StateActionPair(MyState.getNorth(), 0.2, 2));
+			this.piTable[i]
+					.add(new StateActionPair(MyState.getSouth(), 0.2, 3));
 			this.piTable[i].add(new StateActionPair(MyState.getWest(), 0.2, 4));
 			this.piTable[i].add(new StateActionPair(MyState, 0.2, 5));
-			//initialize v table
+			// initialize v table
 			this.vTable[i] = new Double(1.0);
-			//initialize q table
+			// initialize q table
 			Vector<StateActionPair> TempOtherAgentActions = new Vector<StateActionPair>();
-			TempOtherAgentActions.add(new StateActionPair(OtherAgentState.getEast(), 0.0, 1));
-			TempOtherAgentActions.add(new StateActionPair(OtherAgentState.getNorth(), 0.0, 2));
-			TempOtherAgentActions.add(new StateActionPair(OtherAgentState.getSouth(), 0.0, 3));
-			TempOtherAgentActions.add(new StateActionPair(OtherAgentState.getWest(), 0.0, 4));
-			TempOtherAgentActions.add(new StateActionPair(OtherAgentState, 0.0, 5));
+			TempOtherAgentActions.add(new StateActionPair(OtherAgentState
+					.getEast(), 0.0, 1));
+			TempOtherAgentActions.add(new StateActionPair(OtherAgentState
+					.getNorth(), 0.0, 2));
+			TempOtherAgentActions.add(new StateActionPair(OtherAgentState
+					.getSouth(), 0.0, 3));
+			TempOtherAgentActions.add(new StateActionPair(OtherAgentState
+					.getWest(), 0.0, 4));
+			TempOtherAgentActions.add(new StateActionPair(OtherAgentState, 0.0,
+					5));
 
 			int j = 0;
 			this.qTable[i] = new Vector<MMStateActionPair>();
-			for(StateActionPair Aa : this.piTable[i]){
-				for(StateActionPair oAa : TempOtherAgentActions){
-					this.qTable[i].add(new MMStateActionPair(Aa, oAa, 1.0, j++));
+			for (StateActionPair Aa : this.piTable[i]) {
+				for (StateActionPair oAa : TempOtherAgentActions) {
+					this.qTable[i]
+							.add(new MMStateActionPair(Aa, oAa, 1.0, j++));
 				}
-			}	
+			}
 		}
 	}
-	
-	public StateActionPair ChooseAction(Vector<Agent> worldState, double epsilon) {	
+
+	public StateActionPair ChooseAction(Vector<Agent> worldState, double epsilon) {
 		boolean random = (Math.random() < epsilon);
-		if(random){
+		if (random) {
 			double rand = Math.random();
-			double step = 1 / new Double(this.piTable[StateToIndex(worldState)].size());
+			double step = 1 / new Double(
+					this.piTable[StateToIndex(worldState)].size());
 			double counter = step;
 			for (int ii = 0; ii < this.piTable[StateToIndex(worldState)].size(); ii++) {
 				if (counter >= rand) {
 					return this.piTable[StateToIndex(worldState)].elementAt(ii);
 				}
 				counter += step;
-			}			
-		}else{
+			}
+		} else {
 			double rand = Math.random();
-			double counter = new Double(this.piTable[StateToIndex(worldState)].elementAt(0).Value);
+			double counter = new Double(
+					this.piTable[StateToIndex(worldState)].elementAt(0).Value);
 			for (int ii = 0; ii < this.piTable[StateToIndex(worldState)].size(); ii++) {
 				if (counter >= rand) {
 					return this.piTable[StateToIndex(worldState)].elementAt(ii);
 				}
-				counter += this.piTable[StateToIndex(worldState)].elementAt(ii+1).Value;
+				counter += this.piTable[StateToIndex(worldState)]
+						.elementAt(ii + 1).Value;
 			}
 		}
 		return null;
 	}
-	
+
 	public int StateToIndex(Vector<Agent> worldState) {
 		int index = 0;
 		int power = 0;
@@ -166,7 +177,7 @@ public class MMAgent extends Agent {
 		}
 		return index;
 	}
-	
+
 	public int OldStateToIndex(Vector<Agent> worldState) {
 		int index = 0;
 		int power = 0;
@@ -178,7 +189,7 @@ public class MMAgent extends Agent {
 		}
 		return index;
 	}
-	
+
 	public int WhoAmI(Vector<Agent> worldState) {
 		int mySelf = 0;
 		for (int j = 0; j < worldState.size(); j++) {
@@ -198,14 +209,21 @@ public class MMAgent extends Agent {
 						mySelf + 1)));
 		return MyState;
 	}
-	
-	public void UpdateMiniMax(Vector<Agent> worldState, StateActionPair myAction, StateActionPair otherAgentAction, double reward){
-		
-		int oldIndex = OldStateToIndex(worldState);
-		for(int i=0;i<this.qTable[oldIndex].size();i++){
-			if(this.qTable[oldIndex].elementAt(i).myAction.id == myAction.id){
-				if(this.qTable[oldIndex].elementAt(i).otherAction.id == otherAgentAction.id){
-					
+
+	public void UpdateMiniMax(Vector<Agent> worldState,
+			StateActionPair myAction, StateActionPair otherAgentAction,
+			double reward) {
+
+		int oldStateIndex = OldStateToIndex(worldState);
+		int newStateIndex = StateToIndex(worldState);
+		for (int i = 0; i < this.qTable[oldStateIndex].size(); i++) {
+			if (this.qTable[oldStateIndex].elementAt(i).myAction.id == myAction.id) {
+				if (this.qTable[oldStateIndex].elementAt(i).otherAction.id == otherAgentAction.id) {
+					this.qTable[oldStateIndex].elementAt(i).Value = (1 - alpha)
+							* this.qTable[oldStateIndex].elementAt(i).Value
+							+ alpha
+							* (reward + gamma + this.vTable[newStateIndex]);
+					break;
 				}
 			}
 		}
